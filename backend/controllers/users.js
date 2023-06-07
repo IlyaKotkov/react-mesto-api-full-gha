24e06b231cdf292aac37e4204610767c3947696d
Ilya Kotkov <MopsBox2552@yandex.ru>
2023-06-07 20:30:09 +0300

    Поправить код

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -80,7 +80,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret');
 
       res.send({ token });
     })
